3abbdacdf2285df3a018e44aa06d330440df1f4f
woobinda <woobinda@voliacable.com>
2016-11-02 18:19:35 +0200

    fix pep8

--- server/config.py
+++ server/config.py
@@ -1,12 +1,16 @@
-# -*- coding: utf-8 -*- 
-import os, argparse
+# -*- coding: utf-8 -*-
+import os
+import argparse
 from flask import Flask
 
-parser = argparse.ArgumentParser()  # настройка аргументов принимаемых с консоли
+parser = argparse.ArgumentParser()  # настройка принимаемых аргументов
 parser.add_argument("--port", default='7000', type=int, help='Port to listen'),
-parser.add_argument("--hash-algo", default='sha1', type=str, help='Hashing algorithm to use'),
-parser.add_argument("--content-dir", default='UPLOADS', type=str, help='Enable folder to upload'),
-parser.add_argument("--secret", default='d41d8cd98f00b204e9800998ecf8427e', type=str, help='secret key'),
+parser.add_argument("--hash-algo", default='sha1', type=str,
+                    help='Hashing algorithm to use'),
+parser.add_argument("--content-dir", default='UPLOADS',
+                    type=str, help='Enable folder to upload'),
+parser.add_argument("--secret", default='d41d8cd98f00b204e9800998ecf8427e',
+                    type=str, help='secret key'),
 args = parser.parse_args()
 
 port = args.port
@@ -18,9 +22,10 @@ BASE_DIR = os.path.abspath('.')
 
 if not os.path.exists(os.path.join(BASE_DIR,
                                    content_dir)):
-    # если нету папки 'UPLOADS', в которой будут храниться все загрузки, создаем ее
+    # если нету папки 'UPLOADS', в которой будут храниться все загрузки
     os.mkdir(content_dir)
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = os.path.join(BASE_DIR, content_dir)
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # Максимальный размер загружаемых файлов (16 mb)
+# Максимальный размер загружаемых файлов (16 mb)
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
--- server/server.py
+++ server/server.py
@@ -1,17 +1,21 @@
-# -*- coding: utf-8 -*- 
-import os, hashlib, shutil
+import os
+import hashlib
+import shutil
 from flask import request, Response, send_from_directory, jsonify, abort
 from config import *
 
+
 def sha1(stream):
     """
-    The function returns the md5 hash, based on the parameters of the uploaded file.
+    The function returns the md5 hash,
+    based on the parameters of the uploaded file.
     """
     hash_sha1 = hashlib.sha1()
     for chunk in iter(lambda: stream.read(4096), b""):
         hash_sha1.update(chunk)
     stream.seek(0)
     return hash_sha1.hexdigest()
+
 
 def get_size(stream):
     """
@@ -22,24 +26,30 @@ def get_size(stream):
     stream.seek(0)
     return size
 
+
 def make_folder_for_file(file_hash):
     """
     Function determines the folder in which the downloaded file will be placed.
     """
-    path = os.path.join(app.config['UPLOAD_FOLDER'], file_hash[0], file_hash[1])
-    if os.path.exists(path) == False:
-        os.makedirs(os.path.join(app.config['UPLOAD_FOLDER'], file_hash[0], file_hash[1]))
+    path = os.path.join(app.config['UPLOAD_FOLDER'],
+                        file_hash[0], file_hash[1])
+    if not os.path.exists(path):
+        os.makedirs(os.path.join(
+            app.config['UPLOAD_FOLDER'], file_hash[0], file_hash[1]))
     return path
+
 
 def get_token(*args):
     token = ':'.join(str(arg) for arg in args)
-    return hashlib.md5(token).hexdigest() 
+    return hashlib.md5(token).hexdigest()
+
 
 @app.route('/file', methods=['GET', 'POST'])
 def upload_file():
     """
     The function receives a file from client, verifies the token.
-    If token is successful - download a file and return a response with confirm token.
+    If token is successful - download a file
+    and return a response with confirm token.
     """
     if request.method == 'POST':
         file = request.files['file']
@@ -51,12 +61,13 @@ def upload_file():
             file_name = file.filename.encode('utf8')
             client_ip = request.remote_addr
             if token != get_token(file_name, file_size, client_ip,
-                                      args.secret):
+                                  args.secret):
                 abort(404)
             make_folder_for_file(file_hash)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], file_hash[0], file_hash[1], file_hash))
-            confirm_token = get_token(file_name, file_size, client_ip, args.secret,
-                                      file_hash)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], file_hash[
+                      0], file_hash[1], file_hash))
+            confirm_token = get_token(file_name, file_size, client_ip,
+                                      args.secret, file_hash)
             response = jsonify({
                 "file_name": file_name,
                 "file_size": file_size,
@@ -66,39 +77,45 @@ def upload_file():
             return response
         return abort(404)
     return '''
-		<!doctype html>
-		<title>Upload new File</title>		
-		<h1>Upload new File</h1>
-		<form action="" method=post enctype=multipart/form-data>
-		<p><input type=file name=file>
-		<input type="hidden" name="token" value="ca7938276d84240587df58f99f42310d">
-		<input type=submit value=Upload>
-		</form>
-		'''
+        <!doctype html>
+        <title>Upload new File</title>
+        <h1>Upload new File</h1>
+        <form action="" method=post enctype=multipart/form-data>
+        <p><input type=file name=file>
+        <input type="hidden" name="token"
+        value="ca7938276d84240587df58f99f42310d">
+        <input type=submit value=Upload>
+        </form>
+        '''
+
 
 @app.route('/file/<file_hash>')
 def get_file(file_hash):
     """
     Return requested file to client
-    """ 
-    folder = os.path.join(app.config['UPLOAD_FOLDER'], file_hash[0], file_hash[1])
+    """
+    folder = os.path.join(
+        app.config['UPLOAD_FOLDER'], file_hash[0], file_hash[1])
     if folder:
-        return send_from_directory(folder, 
-                        file_hash), {'Content-Type': 'audio/mpeg; charset=utf-8'}
+        return send_from_directory(folder,
+            file_hash), {'Content-Type': 'audio/mpeg; charset=utf-8'}
     else:
         abort(404)
+
 
 @app.route('/file/<file_hash>', methods=['DELETE'])
 def delete_file(file_hash):
     """
-    Delete requested file 
+    Delete requested file
     """
-    file = os.path.join(app.config['UPLOAD_FOLDER'], file_hash[0], file_hash[1], file_hash)
+    file = os.path.join(app.config['UPLOAD_FOLDER'], file_hash[
+                        0], file_hash[1], file_hash)
     if file:
         os.remove(file)
         return 'remove %s' % file_hash
     else:
         abort(404)
+
 
 @app.route('/status', methods=['GET'])
 def get_status():
